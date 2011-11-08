826d24e9e8260efcbc72384d12fab0da1c8ed392
alan bount <alan@zeroasterisk.com>
2011-11-08 13:43:24 -0500

    Implemented @dereuromark's suggested changes (mostly formatting) for cakephp 2.0 compatibility

--- models/behaviors/rangeable.php
+++ models/behaviors/rangeable.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * A CakePHP behavior to facilitate a simple reteival of results via a range search on a lon/lat fields
+ * A CakePHP behavior to facilitate a simple reteival of results 
+ * via a range search on a lon/lat fields
  *
  * Copyright 2010, Alan Blount
  * Alan@zeroasterisk.com
@@ -9,21 +10,22 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright Copyright 2010, Alan Blount
- * @version 1.0
+ * @version 1.1
  * @author Alan Blount <Alan@zeroasterisk.com>
+ * @author dereuromark <https://github.com/dereuromark>
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  * @link https://github.com/zeroasterisk/cakephp-behavior-rangeable
  */
 class RangeableBehavior extends ModelBehavior {
-	var $settings = array();
-	var $milesToDegrees = .01445;
-	var $mapMethods = array('/^_findRange$/' => '_findRange');
-	var $debug = array(); // false or array()
-	
+	public $settings = array();
+	public $milesToDegrees = .01445;
+	public $mapMethods = array('/^_findRange$/' => '_findRange');
+	public $debug = array(); // false or array()
+
 	/**
 	* Setup the Behavior
 	*/
-	function setup(&$Model, $settings = array()) {
+	public function setup(Model $Model, $settings = array()) {
 		if (!isset($this->settings[$Model->alias])) {
 			$this->settings[$Model->alias] = array();
 		}
@@ -34,7 +36,7 @@ class RangeableBehavior extends ModelBehavior {
 			'field_lat' => 'lat',
 			'field_lon' => 'lon',
 			'field_zip' => 'zip', // only used if 'lookup_zip_from_conditions' == true
-			'lookup_zip_from_conditions' => true, // lookup a zip from the conditions array, remove from conditions array 
+			'lookup_zip_from_conditions' => true, // lookup a zip from the conditions array, remove from conditions array
 			'lookup_zip_model_name' => 'Zip',
 			'lookup_zip_field_zip' => 'zip',
 			'lookup_zip_field_lat' => 'lat',
@@ -49,31 +51,31 @@ class RangeableBehavior extends ModelBehavior {
 			'countField' => 'count',
 			), $settings);
 		$Model->_findMethods['range'] = true;
-		
+
 		if (!$Model->hasField($this->settings[$Model->alias]['lookup_zip_field_lat']) || !$Model->hasField($this->settings[$Model->alias]['lookup_zip_field_lon'])) {
 			trigger_error('RangeableBehavior: You must have a valid lat and lon field for querying this Model\'s as rangeable!');
 			die();
 		}
 	}
-	
+
 	/**
 	* Custom Find Method which injects the range box (and optionally increments that range until we hit a minimum set of results) into the conditions
 	* it also re-sorts the results by the distance to the sort point
 	*/
-	function _findRange(&$Model, $method, $state, $query, $results = array()) {
+	public function _findRange(Model $Model, $method, $state, $query, $results = array()) {
 		if ($state == 'before') {
 			$query = array_merge($query, $this->settings[$Model->alias]['query']);
-			
+
 			$lat = $lon = $zip = null;
 			if (isset($query['lat'])) { $lat = $query['lat']; }
-			foreach ( array($this->settings[$Model->alias]['field_lat'], "{$Model->alias}.{$this->settings[$Model->alias]['field_lat']}") as $key ) { 
+			foreach ( array($this->settings[$Model->alias]['field_lat'], "{$Model->alias}.{$this->settings[$Model->alias]['field_lat']}") as $key ) {
 				if (array_key_exists($key, $query['conditions'])) {
 					$lat = $query['conditions'][$key];
 					unset($query['conditions'][$key]);
 				}
 			}
 			if (isset($query['lon'])) { $lon = $query['lon']; }
-			foreach ( array($this->settings[$Model->alias]['field_lon'], "{$Model->alias}.{$this->settings[$Model->alias]['field_lon']}") as $key ) { 
+			foreach ( array($this->settings[$Model->alias]['field_lon'], "{$Model->alias}.{$this->settings[$Model->alias]['field_lon']}") as $key ) {
 				if (array_key_exists($key, $query['conditions'])) {
 					$lon = $query['conditions'][$key];
 					unset($query['conditions'][$key]);
@@ -81,7 +83,7 @@ class RangeableBehavior extends ModelBehavior {
 			}
 			if (isset($query['zip'])) { $zip = $query['zip']; }
 			if ($this->settings[$Model->alias]['lookup_zip_from_conditions']) {
-				foreach ( array($this->settings[$Model->alias]['field_zip'], "{$Model->alias}.{$this->settings[$Model->alias]['field_zip']}") as $key ) { 
+				foreach ( array($this->settings[$Model->alias]['field_zip'], "{$Model->alias}.{$this->settings[$Model->alias]['field_zip']}") as $key ) {
 					if (array_key_exists($key, $query['conditions'])) {
 						$zip = $query['conditions'][$key];
 						unset($query['conditions'][$key]);
@@ -92,7 +94,7 @@ class RangeableBehavior extends ModelBehavior {
 						if (!isset($this->ZipModel) || !is_object($this->ZipModel)) {
 							App::import('Model', $this->settings[$Model->alias]['lookup_zip_model_name']);
 							$this->ZipModel = null;
-							$this->ZipModel =& ClassRegistry::init($this->settings[$Model->alias]['lookup_zip_model_name']);
+							$this->ZipModel = ClassRegistry::init($this->settings[$Model->alias]['lookup_zip_model_name']);
 						}
 						$zipData = $this->ZipModel->find('first', array(
 							'recursive' => 1,
@@ -134,7 +136,7 @@ class RangeableBehavior extends ModelBehavior {
 				if (isset($query['limitless'])) { $limitless = $query['limitless']; }
 				$order_by_distance = $this->settings[$Model->alias]['order_by_distance'];
 				if (isset($query['order_by_distance'])) { $order_by_distance = $query['order_by_distance']; }
-				if (!empty($limitless) && !empty($order_by_distance) && isset($query['limit'])) { 
+				if (!empty($limitless) && !empty($order_by_distance) && isset($query['limit'])) {
 					$query['limit_'] = $query['limit'];
 					$query['limit'] = (is_numeric($limitless) ? $limitless : 9999);
 				}
@@ -150,7 +152,7 @@ class RangeableBehavior extends ModelBehavior {
 		if (isset($query['lon'])) { $lon = $query['lon']; }
 		$order_by_distance = $this->settings[$Model->alias]['order_by_distance'];
 		if (isset($query['order_by_distance'])) { $order_by_distance = $query['order_by_distance']; }
-		
+
 		if (!empty($lat) && !empty($lon) && !empty($order_by_distance)) {
 			$resultsByRange = array();
 			foreach ( $results as $result ) {
@@ -171,13 +173,13 @@ class RangeableBehavior extends ModelBehavior {
 					// treated as a string, so that putting into an array doesn't force type to an int.
 					$distanceSortable = strval($distanceSortable);
 					$distanceSortable = substr($distanceSortable, 0, strlen($distanceSortable)-3).'.'.substr($distanceSortable,-3, 4);
-					$results[$distanceSortable] = $result; 
+					$results[$distanceSortable] = $result;
 				}
 			}
 		}
 		return $results;
 	}
-	
+
 	/**
 	* Get the latitude and longitude for a single zip code.
 	*
@@ -189,7 +191,7 @@ class RangeableBehavior extends ModelBehavior {
 	* @param float $lon
 	* @return array conditions node for this range
 	*/
-	function getRangeQuery(&$Model, $query, $range, $lat, $lon) {
+	public function getRangeQuery(Model $Model, $query, $range, $lat, $lon) {
 		$maxCoords = $this->getRangeBox($range, $lat, $lon);
 		$return = $query;
 		$return['conditions']["{$Model->alias}.{$this->settings[$Model->alias]['lookup_zip_field_lat']} BETWEEN ? AND ?"] = array($maxCoords['min_lat'], $maxCoords['max_lat']);
@@ -197,7 +199,7 @@ class RangeableBehavior extends ModelBehavior {
 		if (is_array($this->debug)) {
 			$this->debug[__function__][] = compact('range', 'query', 'lat', 'lon', 'return');
 		}
-		return $return; 
+		return $return;
 	}
 	/**
 	* Get the maximum and minimum longitude and latitude values
@@ -214,7 +216,7 @@ class RangeableBehavior extends ModelBehavior {
 	* @param float $lon
 	* @return array associative index max/min lat/lon
 	*/
-	function getRangeBox($range, $lat, $lon) {
+	public function getRangeBox($range, $lat, $lon) {
 		// Calculate the degree range using the mile range
 		$degrees = $range * $this->milesToDegrees;
 		$lat = floatval($lat);
@@ -228,10 +230,10 @@ class RangeableBehavior extends ModelBehavior {
 		if (is_array($this->debug)) {
 			$this->debug[__function__][] = compact('range', 'lat', 'lon', 'return');
 		}
-		return $return; 
+		return $return;
 	}
-	
-	
+
+
 	/**
 	* this does the math to find the actual distance between two sets of lat/lon coordinates
 	* @param float $sourceLat
@@ -241,15 +243,14 @@ class RangeableBehavior extends ModelBehavior {
 	* @param int $precision
 	* @return float $distanceBetweenSourceAndTarget
 	*/
-	function calculatePrecisionDistance($sourceLat, $sourceLon, $targetLat, $targetLon, $precision = 2) {
+	public function calculatePrecisionDistance($sourceLat, $sourceLon, $targetLat, $targetLon, $precision = 2) {
 		$earthsradius = 3963.19;
 		$pi = pi();
 		$c = sin($sourceLat/(180/$pi)) * sin($targetLat/(180/$pi)) +
 		cos($sourceLat/(180/$pi)) * cos($targetLat/(180/$pi)) *
-        cos($targetLon/(180/$pi) - $sourceLon/(180/$pi));
+		cos($targetLon/(180/$pi) - $sourceLon/(180/$pi));
 		$c = preg_replace("/1/", "1", $c);
 		$distance = $earthsradius * acos($c);
 		return round($distance, $precision);
 	}
 }
-?>
